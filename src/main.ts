849c1c5e9e8b87038a7e847e0985ecc4a2d63e1a
Juansecu <juansecoder@hotmail.com>
2024-02-17 00:05:37 -0500

    SECURITY - Check if client is GitHub when processing webhook requests
    Close #5

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,58 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import { Logger } from 'winston';
 
 import { ConsoleLogger } from './loggers/console.logger';
 
 import { RequestHandler } from './handlers/request.handler';
 
 const app: Express = express();
+const consoleLogger: Logger = ConsoleLogger.getLogger('main');
 const port: string = process.env.PORT ?? '3000';
 
 app.post(
   '/webhook',
   express.json({ type: 'application/json' }),
+  checkIfClientIsGitHub,
   (request: Request, response: Response): void => {
     new RequestHandler().handleRequest(request, response);
   }
 );
 
 app.listen(port, (): void => {
-  ConsoleLogger.getLogger('main').info(`Listening on port ${port}`);
+  consoleLogger.info(`Listening on port ${port}`);
 });
+
+function checkIfClientIsGitHub(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  consoleLogger.verbose('Checking if client is GitHub...');
+
+  if (process.env.NODE_ENV === 'production') {
+    if (request.get('host') !== 'api.github.com') {
+      consoleLogger.error('Client is not GitHub');
+      response.status(403).send('Forbidden');
+      return;
+    }
+  }
+
+  if (
+    !request.get('hostname') ||
+    !request.get('User-Agent') ||
+    !request.get('X-GitHub-Event')
+  ) {
+    consoleLogger.error('Client is not GitHub');
+    response.status(403).send('Forbidden');
+    return;
+  }
+
+  if (
+    request.get('hostname') === 'GitHub' &&
+    request.get('User-Agent')!.startsWith('GitHub-Hookshot/') &&
+    request.get('X-GitHub-Event')
+  ) {
+    consoleLogger.info('Client is GitHub');
+    next();
+  }
+}
