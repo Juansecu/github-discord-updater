1dd1559e8578f1e29b2a57928074c0383b87cb0e
Juansecu <43616657+Juansecu@users.noreply.github.com>
2025-06-08 00:36:13 -0500

    CONFIG - Update ESLint config to version 9

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { description, version } = require('../../package.json');
 
 import { Options } from 'swagger-jsdoc';
